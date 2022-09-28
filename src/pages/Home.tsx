a91fe09700c091361e293dc75fd9274a9417f810
那可心 <nakx@raysdata.com>
2022-09-28 15:03:48 +0800

    feat: init

---
+++ src/pages/Home.tsx
@@ -0,0 +1,15 @@
+const Home = () => {
+  return (
+    <div>
+      <h1>hello-ssr</h1>
+      <button
+        onClick={(): void => {
+          alert('hello-ssr');
+        }}>
+        alert
+      </button>
+    </div>
+  );
+};
+
+export default Home;
