b04557062be1b7c1d193ddefb6df2df3ba96c7a2
那可心 <nakx@raysdata.com>
2022-10-07 22:03:51 +0800

    ssr支持请求

--- src/pages/Demo.tsx
+++
@@ -1,7 +0,0 @@
-import { FC } from 'react';
-
-const Demo: FC = () => {
-  return <div>这是一个demo页面</div>;
-};
-
-export default Demo;
--- src/server/index.tsx
+++ src/server/index.tsx
@@ -3,39 +3,82 @@ import childProcess from 'child_process';
 import { renderToString } from 'react-dom/server';
 import path from 'path';
 import router from '@/router';
-import { Route, Routes } from 'react-router-dom';
+import { Route, Routes, matchRoutes, RouteObject } from 'react-router-dom';
 import { StaticRouter } from 'react-router-dom/server';
 import { Helmet } from 'react-helmet';
+import { serverStore } from '@/store';
+import { Provider } from 'react-redux';
 
 const app = express();
 
+const bodyParser = require('body-parser');
+
 app.use(express.static(path.resolve(process.cwd(), 'client_build')));
 
+// 请求body解析
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.post('/api/getDemoData', (req, res) => {
+  res.send({
+    data: req.body,
+    status_code: 0
+  });
+});
+
 app.get('*', (req, res) => {
-  const content = renderToString(
-    <StaticRouter location={req.path}>
-      <Routes>
-        {router?.map((item, index) => {
-          return <Route {...item} key={index} />;
-        })}
-      </Routes>
-    </StaticRouter>
-  );
+  const routeMap = new Map<string, () => Promise<any>>(); // path - loaddata 的map
+  router.forEach(item => {
+    if (item.path && item.loadData) {
+      routeMap.set(item.path, item.loadData(serverStore));
+    }
+  });
 
-  const helmet = Helmet.renderStatic();
+  // 匹配当前路由的routes
+  const matchedRoutes = matchRoutes(router as RouteObject[], req.path);
 
-  res.send(`
-    <html>
-      <head>
-        ${helmet.title.toString()}
-        ${helmet.meta.toString()}
-      </head>
-      <body>
-        <div id="root">${content}</div>
-        <script src="/index.js"></script>
-      </body>
-    </html>
+  const promises: Array<() => Promise<any>> = [];
+  matchedRoutes?.forEach(item => {
+    if (routeMap.has(item.pathname)) {
+      promises.push(routeMap.get(item.pathname) as () => Promise<any>);
+    }
+  });
+
+  Promise.all(promises).then(data => {
+    // 统一放到state里
+    // 编译需要渲染的JSX, 转成对应的HTML STRING
+    const content = renderToString(
+      <Provider store={serverStore}>
+        <StaticRouter location={req.path}>
+          <Routes>
+            {router?.map((item, index) => {
+              return <Route {...item} key={index} />;
+            })}
+          </Routes>
+        </StaticRouter>
+      </Provider>
+    );
+
+    const helmet = Helmet.renderStatic();
+
+    res.send(`
+      <html>
+        <head>
+          ${helmet.title.toString()}
+          ${helmet.meta.toString()}
+        </head>
+        <body>
+          <div id="root">${content}</div>
+          <script>
+            window.context = {
+              state: ${JSON.stringify(serverStore.getState())}
+            }
+          </script>
+          <script src="/index.js"></script>
+        </body>
+      </html>
   `);
+  });
 });
 
 app.listen(3000, () => {
